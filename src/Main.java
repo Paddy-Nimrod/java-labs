b4e31d3047843809d8f6cdb8637fb1ec55dade4e
Paddy-Nimrod <n.ambetsa21@gmail.com>
2023-08-14 05:34:28 +0300

    added generics from the raw code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> items = new ArrayList<Integer>();
+
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+        items.add(5);
+
+        printDoubled(items);
     }
+
+    private static void printDoubled(ArrayList<Integer> items) {
+        for (int i : items) {
+            System.out.println(i);
+        }
+    }
+
 }
